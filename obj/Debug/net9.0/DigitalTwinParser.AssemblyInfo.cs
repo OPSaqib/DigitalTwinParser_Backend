be470039e6bfacf4acffc80b37a4b669a7d8156d
sansh <s.malhotra1@student.tue.nl>
2025-04-28 12:33:21 +0200

    backEndMain test case file has been added

--- obj/Debug/net9.0/DigitalTwinParser.AssemblyInfo.cs
+++ obj/Debug/net9.0/DigitalTwinParser.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DigitalTwinParser")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+02dff84587f2df4d0d9ab128fe07f9dfa54655ff")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3d675c0b8e96ecb1d3163ee2e60acbce42c7f6e4")]
 [assembly: System.Reflection.AssemblyProductAttribute("DigitalTwinParser")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DigitalTwinParser")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
