22b36cc5cade832b730a8e7fcb0d4f5071633dad
OPSaqib <131691880+OPSaqib@users.noreply.github.com>
2025-04-28 14:30:04 +0200

    Update Asset.cs

--- Asset.cs
+++ Asset.cs
@@ -1,7 +1,19 @@
+ public class Asset {
+        public string Description {get; set;}
+        public List<string> SpecializationOf {get; set;}
+        public List<string> CompositionOf {get; set;}
+        public Dictionary<string, object> Associations {get; set; }
+        public AssetAttributes Attributes {get; set;}
 
-public class Asset {
-
-    Asset() {
-                    
+        public Asset() {
+            SpecializationOf = new List<string>();
+            CompositionOf = new List<string>();
+            Associations = new Dictionary<string, object>();
+            Attributes = new AssetAttributes();
+        }
     }
-}
+
+    public class AssetAttributes {
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
