ea49584d1cc58ed8a593b39aaa8c292cbe7b5a14
Saqib Khan <opsaqib@gmail.com>
2025-04-29 10:53:19 +0200

    Updated and improved backendmain

--- BackendMain.cs
+++ BackendMain.cs
@@ -9,9 +9,9 @@ class BackendMain {
     @input: String[]
     */
     static List<Entity> addAsset(List<Entity> assets, string[] parameters) {
-        assets.Add(new Entity());
-        int length = assets.Count();
-        assets[length] = buildAsset(assets[length], parameters);
+        Entity asset = new Entity();
+        asset = buildAsset(asset, parameters);
+        assets.Add(asset);
         return assets;
     }
 
@@ -22,8 +22,27 @@ class BackendMain {
     @output: Entity 
     */
     static Entity buildAsset(Entity asset, string[] parameters) {
-        //TODO:
+        /*
+        Assuming parameter mapping like:
+        parameters[0] = Description
+        parameters[1] = SpecializationOf (comma-separated)
+        parameters[2] = CompositionOf (comma-separated)
+        parameters[3] = Attribute Name
+        parameters[4] = Attribute Description
+        parameters[5] = Attribute ValueType
+        parameters[6] = Attribute Value
+        */
         
+        if (parameters.Length > 0) asset.Description = parameters[0];
+        if (parameters.Length > 1 && !string.IsNullOrEmpty(parameters[1]))
+            asset.SpecializationOf = parameters[1].Split(',').Select(s => s.Trim()).ToList();
+        if (parameters.Length > 2 && !string.IsNullOrEmpty(parameters[2]))
+            asset.CompositionOf = parameters[2].Split(',').Select(s => s.Trim()).ToList();
+        if (parameters.Length > 3) asset.Attributes.Name = parameters[3];
+        if (parameters.Length > 4) asset.Attributes.Description = parameters[4];
+        if (parameters.Length > 5) asset.Attributes.ValueType = parameters[5];
+        if (parameters.Length > 6) asset.Attributes.Value = parameters[6];
+
         return asset;
     }
 }
