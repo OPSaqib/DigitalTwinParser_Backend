f7f032241b22b3ecb7a23985f1be5f5e86d2e3ab
OPSaqib <131691880+OPSaqib@users.noreply.github.com>
2025-05-01 22:57:56 +0200

    Major Changes To BackendMain Based On Object

--- BackendMain.cs
+++ BackendMain.cs
@@ -4,119 +4,82 @@ using System.ComponentModel.DataAnnotations;
 public class BackendMain {
 
     /*
-    Description: Method to instantiate an Asset 
-    @output: List<Asset>
-    @input: String[] parameters
+    Description: Method to instantiate an Concept
+    @input: String[] parameters (with the data), List<Concept>
+    @output: List<Concept>
     */
-    public static List<Asset> addAsset(List<Asset> assets, string[] parameters) {
-        Asset asset = buildAsset(parameters);
-        assets.Add(asset);
-        return assets;
+    public static List<Concept> addConcept(List<Concept> concepts, string[] parameters) {
+        Concept newConcept = buildConcept(parameters);
+        concepts.Add(newConcept);
+        return concepts;
     }
 
     /*
-    Description: Method to build an Asset object from parameters
-    @input: string[] parameters
-    @output: Asset
+    Description: Method to build an Concept object from parameters
+    @input: string[] parameters (with the data)
+    @output: Concept
     */
-    private static Asset buildAsset(string[] parameters) {
-        // Assumes parameters:
-        // [0-6] => Entity
-        // [7-11] => Relation
-        // [12-17] => ValueType
+    private static Concept buildConcept(string[] parameters) {
+        Concept concept = new Concept();
 
-        Entity entity = buildEntity(parameters);
-        Relation relation = buildRelation(parameters);
-        ValueType valueType = buildValueType(parameters);
+        // MODIFY IT BASED ON HOW THE PARAMTERS LOOKS BELOW IS JUST SOME AI GENERATED EXAMPLE
+        
+        // Assuming parameters array contains at least Name and Description
+        //if (parameters.Length >= 2) {
+            //concept.Name = parameters[0];
+            //concept.Description = parameters[1];
+        //}
 
-        return new Asset(entity, relation, valueType);
+        // Handle SubConcepts if provided in parameters (assuming format: name1,desc1,name2,desc2,...)
+        //if (parameters.Length > 2) {
+            //for (int i = 2; i < parameters.Length; i += 2) {
+                //if (i + 1 < parameters.Length) {
+                    //SubConcept subConcept = new SubConcept();
+                    //subConcept.Name = parameters[i];
+                    //subConcept.Description = parameters[i + 1];
+                    //concept.SubConcepts.Add(subConcept.Name, subConcept);
+                //}
+            //}
+        //}
+
+        return concept;
     }
 
     /*
-    Description: Method to build an entity object and initialize it with parameters
+    Description: Method to add a sub-concept to an already existing concept
+    @input: List<Concept>, string concept_name, string subConcept
+    @output: List<Concept>
     */
-    private static Entity buildEntity(string[] parameters) {
-        Entity entity = new Entity();
-
-        if (parameters.Length > 0) entity.Description = parameters[0];
-
-        if (parameters.Length > 1 && !string.IsNullOrEmpty(parameters[1]))
-            entity.SpecializationOf = parameters[1].Split(',').Select(s => s.Trim()).ToList();
-
-        if (parameters.Length > 2 && !string.IsNullOrEmpty(parameters[2]))
-            entity.CompositionOf = parameters[2].Split(',').Select(s => s.Trim()).ToList();
-
-        if (parameters.Length > 3) entity.Attributes.Name = parameters[3];
-        if (parameters.Length > 4) entity.Attributes.Description = parameters[4];
-        if (parameters.Length > 5) entity.Attributes.ValueType = parameters[5];
-        if (parameters.Length > 6) entity.Attributes.Value = parameters[6];
-
-        return entity;
+    public static List<Concept> addSubConcept(List<Concept> concepts, string concept_name, string subConcept) {
+        foreach (Concept concept in concepts) {
+            if (concept.Name == concept_name) {
+                SubConcept newSubConcept = new SubConcept();
+                newSubConcept.Name = subConcept;
+                concept.SubConcepts.Add(subConcept, newSubConcept);
+                break;
+            }
+        }
+        return concepts;
     }
 
     /*
-    Description: Method to build a Relation object from parameters
+    Description: Method to modify the ‘examples’ list in a defined subconcept in a concept
+    @input: List<Concept>, string concept_name, string subConcept, List<String> examples
+    @output: List<Concept>
     */
-    private static Relation buildRelation(string[] parameters) {
-        Relation relation = new Relation();
-
-        if (parameters.Length > 7) relation.AltName = parameters[7];
-        if (parameters.Length > 8) relation.Description = parameters[8];
-        if (parameters.Length > 9) relation.Cardinality = parameters[9];
-        if (parameters.Length > 10) relation.Role = parameters[10];
-        if (parameters.Length > 11) relation.InverseRole = parameters[11];
-
-        return relation;
+    public static List<Concept> modifyExamples(List<Concept> concepts, string concept_name, string subConcept, List<string> examples) {
+        foreach (Concept concept in concepts) {
+            if (concept.Name == concept_name) {
+                if (concept.SubConcepts.ContainsKey(subConcept)) {
+                    concept.SubConcepts[subConcept].Examples.Clear();
+                    concept.SubConcepts[subConcept].Examples.AddRange(examples);
+                    break;
+                }
+            }
+        }
+        return concepts;
     }
 
-    /*
-    Description: Method to build a ValueType object from parameters
-    */
-    private static ValueType buildValueType(string[] parameters) {
-        ValueType valueType = new ValueType();
-
-        if (parameters.Length > 12) valueType.Type = parameters[12];
-        if (parameters.Length > 13) valueType.Description = parameters[13];
-
-        if (parameters.Length > 14 && double.TryParse(parameters[14], out double min))
-            valueType.Min = min;
-
-        if (parameters.Length > 15 && double.TryParse(parameters[15], out double max))
-            valueType.Max = max;
-
-        if (parameters.Length > 16) valueType.Example = parameters[16];
-
-        if (parameters.Length > 17 && !string.IsNullOrEmpty(parameters[17]))
-            valueType.AllowedValues = parameters[17].Split(',').Select(s => s.Trim()).ToList();
-
-        return valueType;
-    }
-
-        /*
-    Example input string array:
-    string[] parameters = new string[] {
-        "Electric Motor",                              // [0] Entity.Description
-        "RotatingMachine,Electromechanical",           // [1] Entity.SpecializationOf
-        "MotorHousing,DriveShaft",                     // [2] Entity.CompositionOf
-        "MotorID",                                      // [3] Entity.Attributes.Name
-        "Unique identifier for the motor",             // [4] Entity.Attributes.Description
-        "string",                                       // [5] Entity.Attributes.ValueType
-        "EM-12345",                                     // [6] Entity.Attributes.Value
-
-        "has a",                                        // [7] Relation.AltName
-        "Motor has a unique identifier",               // [8] Relation.Description
-        "1..1",                                         // [9] Relation.Cardinality
-        "owner",                                        // [10] Relation.Role
-        "owned",                                        // [11] Relation.InverseRole
-
-        "string",                                       // [12] ValueType.Type
-        "ID as a string",                               // [13] ValueType.Description
-        "0",                                            // [14] ValueType.Min
-        "",                                             // [15] ValueType.Max (blank/null)
-        "EM-12345",                                     // [16] ValueType.Example
-        ""                                              // [17] ValueType.AllowedValues (optional, comma-separated)
-    };
-    */
 }
 
 
